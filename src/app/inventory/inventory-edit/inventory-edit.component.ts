4ad3491a0bbe5e1813cafd71390158353bae7005
JDeVoldemort <104032188+JDeVoldemort@users.noreply.github.com>
2024-04-03 15:13:52 -0600

    add structure for pages

---
+++ src/app/inventory/inventory-edit/inventory-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './inventory-edit.component.html',
+  styleUrl: './inventory-edit.component.css'
+})
+export class InventoryEditComponent {
+
+}
